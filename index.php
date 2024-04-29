e08949e13db6cade5014cd6c58862ddecdbc2489
Harish <harishkumarsp023@gmail.com>
2024-04-29 17:30:23 +0530

    update CSS styles

--- index.php
+++ index.php
@@ -1,45 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacking Lab</title>
-    <link rel="stylesheet" href="assets/css/output.css">
-    <title>Bootstrap demo</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
-    <!-- <style>
-        .card {
-            position: relative;
-        }
 
-        .card p {
-            position: absolute;
-            top: 20px;
-            left: 5;
-            background-color: rgba(0, 0, 0, 0.7);
-            color: white;
-            padding: 30px 10px;
-            margin: 0;
-            font-size: 14px;
-        }
-    </style> -->
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/index.css">
 </head>
+
 <body>
-    <h1>Welcome to live Practice Machine</h1>
-   
-    <div class="card bg-orange-600" style="width: 18rem;">
-    <div style="">
-    <p>Attack the Home</p>
+    <div class="container-warp">
+<!-- ========== Start navbar ========== -->
+<nav class="navbar navbar-expand-lg bg-navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./assets/logo.png" alt="Bootstrap" width="80" height="60">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item mx-3">
+          <a class="nav-link " aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="#">Chanage Lab</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="#">Help</a>
+        </li>
+        
+      </ul>
     </div>
-        <!-- <img src="./assets/labthumline/i2.jpeg" class="card-img-top" alt="..."> -->
-       
-        <div class="card-body">
-            <h5 class="card-title">XSS Live Lab</h5>
-            <a href="xss.php" class="">Attack web</a>
-            <a href="xss.php" class="">Learn xss</a>
+  </div>
+</nav>
+<!-- ========== End navbar ========== -->
+    
+
+<div class="home-backgourd-cover"></div>
+
+<!-- ========== Start main ========== -->
+<section class="main container-fluid d-flex align-items-center justify-content-center " id="main" style="margin-top: 60px; margin-bottom: 170px;">
+    <div class="d-flex">
+        <div>
+            <img src="./assets/logo-main.svg" alt="" width="300" height="400">
+        </div>
+        <div class="d-flex align-items-center ml-3">
+            <div class="d-flex-col">
+            <h1 class="main-h1">Welcome to <span class="main-h1-span">கவசம்</span></h1>
+            
+            <a href="./php/xss.php" class="btn btn-shin mt-3">Challange</a>
+            </div>
+           
         </div>
     </div>
-    
+</section>
+<!-- ========== End main ========== -->
+<hr class="kavasam-hr">
+<section class="main container-fluid about text-center ">
+<h1 class="about-h1">About <span class="main-h1-span pt-2">Kavasam</span></h1>
+<p class="about-p mt-4 " style="padding-left: 300px;padding-right: 300px;">
+Welcome to Kavasam, your ultimate shield in the digital world. We provide a robust platform for cybersecurity enthusiasts to learn, practice, and master various aspects of cybersecurity. Our live practice labs offer real-world scenarios to test and enhance your skills. Whether you're a beginner looking to explore the field or an experienced professional seeking to stay on top of the latest threats, Kavasam is your go-to resource for all things cybersecurity. 
+</p>
+</section>
+
+
+</div>
+     <!-- <h1>Welcome to live Practice Machine</h1>
    
+    <div class="card" style="width: 18rem;">
+        <img src="./assets/labthumline/i2.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Xss Payload</h5>
+            <a href="#" class="btn btn-primary">Try Xss</a>
+            <a href="#" class="btn btn-primary">Learn Xss</a>
+        </div>
+    </div>  -->
+
+
+    
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
---
+++ info.md
@@ -0,0 +1,4 @@
+## To run Tailwindcss 
+``` javascipt
+ npx tailwindcss -i ./assets/css/input.css -o ./assets/css/output.css --watch
+ ```
